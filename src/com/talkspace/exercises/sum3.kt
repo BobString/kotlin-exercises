a3ef2830cf8266f7565250a841432abcea710efc
Roberto Martin <robert.90@gmail.com>
2018-09-28 13:48:49 -0400

    sum 2 and 3 optimized

--- src/com/talkspace/exercises/sum3.kt
+++ src/com/talkspace/exercises/sum3.kt
@@ -3,10 +3,10 @@ package com.talkspace.exercises
 
 // Write a function that takes as its arguments a sorted array of unique integers, `intArray`, and an integer, `int`, and returns 3 integers from `intArray` that sum up to `int`.
 fun main(args: Array<String>) {
-    println(threeSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [2, 5, -1]
+    println(threeSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [4, 3, -1]
 
-    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [-1, 7]
-    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 20)) // [8, 12]
+    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [4, 2]
+    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 20)) // [12, 8]
 
 }
 
@@ -29,11 +29,15 @@ fun threeSum(ints: List<Int>, total: Int): List<Int> {
 
 
 fun twoSum(ints: List<Int>, total: Int): List<Int> {
+    val hash = mutableMapOf<Int, Int>()
+
     for (integer in ints) {
         val matchingSolution = total - integer
 
-        if (ints.contains(matchingSolution)) {
+        if (hash.containsKey(matchingSolution)) {
             return listOf(integer, matchingSolution)
+        } else {
+            hash[integer] = matchingSolution
         }
     }
 
