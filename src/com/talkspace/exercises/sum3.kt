9cd111fa9a2dfaea0d71bf18f8ea2309023b4ad1
Roberto Martin <robert.90@gmail.com>
2018-09-28 13:24:23 -0400

    sum 3 v2

--- src/com/talkspace/exercises/sum3.kt
+++ src/com/talkspace/exercises/sum3.kt
@@ -3,10 +3,10 @@ package com.talkspace.exercises
 
 // Write a function that takes as its arguments a sorted array of unique integers, `intArray`, and an integer, `int`, and returns 3 integers from `intArray` that sum up to `int`.
 fun main(args: Array<String>) {
-    println(threeSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6))
+    println(threeSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [-1, 8, -1]
 
-    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // -1, 7
-    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 20)) // 8, 12
+    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 6)) // [-1, 7]
+    println(twoSum(listOf(-1, 2, 3, 4, 5, 7, 8, 12), 20)) // [8, 12]
 
 }
 
